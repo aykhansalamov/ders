dc557f2e68f9b39a1fac938d10ca82b34ea88a49
aykhansalamov <aykhan.salamov@abb-bank.az>
2025-05-03 19:11:30 +0400

    Lesson 24

--- src/main/java/org/example/lesson23/BinarySearchExample.java
+++ src/main/java/org/example/lesson23/BinarySearchExample.java
@@ -2,18 +2,40 @@ package org.example.lesson23;
 
 public class BinarySearchExample {
     public static void main(String[] args) {
-        int[] arr = {1, 3, 5, 7, 9};
-        int target = 7;
-        int left = 0, right = arr.length - 1;
 
+        //           0, 1, 2, 3, 4
+        int[] arr = {1, 3, 5, 7, 9}; // array 1, 3, 5, 7, 9
+
+        //  hedef
+        int target = 7; // 7-ni kolda tap (indeksini)
+
+        //  sol  = 0.  sag  = ar. uzunluq - 1  (indeks nomresi)
+        int left = 0, right = arr.length - 1; // 4
+
+     // iken ( sol <= sag)
         while (left <= right) {
+
+        //     orta =  (4    +  0)   / 2 = 2;
             int mid = (right + left) / 2;
+
+        // eger(   5     =   7)
             if (arr[mid] == target) {
+
+            //    cap et
                 System.out.println("Tapildi: " + mid);
+                System.out.printf("Eded ozu: " + arr[mid]);
                 break;
+
+        //   yox, eger ( 5      <    7  )
             } else if (arr[mid] < target) {
+
+         //      sol = 2 + 1;
                 left = mid + 1;
+
+        //    yoxsa
             } else {
+
+         //      sag  = sol - 1;
                 right = mid - 1;
             }
         }
--- src/main/java/org/example/lesson23/BubbleSortExample.java
+++ src/main/java/org/example/lesson23/BubbleSortExample.java
@@ -5,16 +5,32 @@ import java.util.Arrays;
 public class BubbleSortExample {
     public static void main(String[] args) {
         int[] numbers = {5, 3, 1};
- //                      3, 3, 1
+        //               3, 1, 5
+        //               1, 3, 5
+ //    indeks   0 < 1
+  //   eded     5 > 3                   temp=5
+  //            3 = 3
+  //            3   5
 //                       3, 5, 1
-        for (int i = 0; i < numbers.length-1; i++) {
-            for (int j = 0; j < numbers.length-i-1; j++) {
+        //               3, 1, 5
+        //               1, 3, 5
+        //
+        //      {0, 1, 2, 3, 4, 5, 6}  7
+        //      {7, 3, 2, 5, 9, 6, 0}
+        //          7  7  7  6  9  9
+        //      {0, 2, 3, 5, 6, 7, 9}
+
+        for (int i = 0; i < numbers.length-1; i++) {  // indeks sayi qeder tekrar et  //  36 -> 25 //
+
+            for (int j = 0; j < numbers.length-i-1; j++) {  // 0-la 1-i muqayise et, 1-le 2-ni muqayise et, 2 ile 3-w muqayise et ....
                 //     5       >    3
-                if (numbers[j] > numbers[j+1]) {
+                if (numbers[j] > numbers[j+1]) {  // sol sagdan boyukdurse yerlerini yeshis
                 //      temp =     5
                     int temp = numbers[j];
+
       //            numbers[0] =   3
                     numbers[j] = numbers[j+1];
+
        //           numbers[1]   = 5;
                     numbers[j+1] = temp;
                 }
