bf48a66de29cc07f9365ab9910a623a4c4f4eec1
aykhansalamov <aykhan.salamov@abb-bank.az>
2025-05-06 20:55:41 +0400

    Lesson 25

---
+++ src/main/java/org/example/lesson24/DevelopmentTools.java
@@ -0,0 +1,14 @@
+package org.example.lesson24;
+
+
+import lombok.Data;
+import lombok.Getter;
+
+@Data
+@Getter
+public class DevelopmentTools {
+    public static void main(String[] args) {
+
+
+    }
+}
